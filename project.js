1823819f582b73d079e1b344406017c65b1f6394
Benjamin Wilson <wils0859@gmail.com>
2017-08-28 16:53:27 -0600

    Another commit, working on card generation

--- project.js
+++ project.js
@@ -1,47 +1,52 @@
 $(document).ready(() => {
-
-// Make the deck
+  deckID = ''
+  // Make the deck
   let renderDeck = $.getJSON('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-
   renderDeck.done(function(data) {
       if (renderDeck.status !== 200) {
-          return;
+        return;
       }
-
       console.log(data);
 
-// Isolate the deck ID for use in further AJAX calls
-  let deckID = data["deck_id"]
+
+  // Isolate the deck ID for use in further AJAX calls
+  deckID = data["deck_id"]
   console.log(deckID)
 
-// Deal a card
+
+
+  //Deal a card
+  // $('hit-me').click(() => {
   let dealCard = $.getJSON('https://deckofcardsapi.com/api/deck/' + deckID + '/draw/?count=1');
-
-  let a = 'https://deckofcardsapi.com/api/deck/' + deckID + '<<deck_id>>/draw/?count=1'
-  console.log(a)
   dealCard.done(function(data) {
-      if (dealCard.status !== 200) {
-          return;
-      }
+  if (dealCard.status !== 200) {
+    return;
+  }
+    let cardImg = (dealCard.responseJSON.cards[0].image)
 
-      console.log(data);
+    console.log(cardImg)
 
-      let newCard = data
-
-// Shuffle the deck
-  let shuffleDeck = $.getJSON('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-  shuffleDeck.done(function(data) {
-    if (shuffleDeck.status !== 200) {
-        return;
-    }
-
-    console.log(data);
-
-    let playAgain = data
+    $('#dealers-cards').append(<img src=cardImg>)
 
 
+  })// Let the dealer play it out
 
-  })
-  })
-  })
-})
+
+  // Calculate to see who wins
+
+
+  // Shuffle the deck
+  // $('#new-hand').click((event) => {
+  //   event.preventDefault
+  //   let shuffleDeck = $.getJSON('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+  //   shuffleDeck.done(function(data) {
+  //   if (shuffleDeck.status !== 200) {
+  //     return;
+  //   }
+  //     console.log(data)
+  //           })
+  //         })
+  //       })
+      })
+    })
+  //  })
